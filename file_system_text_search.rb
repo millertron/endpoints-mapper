53b090bd73d926b4027cb8ae032cf421b9025ea4
Millertron <senjinkoha@gmail.com>
2019-03-18 15:23:54 +0000

    Configured Rubocop
    Added rubocop in gemfile, made some config and fixed existing code

--- file_system_text_search.rb
+++ file_system_text_search.rb
@@ -1,3 +1,8 @@
-(Dir["./search_directory/**/*.vm"]+Dir["./search_directory/**/*.js"]-Dir["./search_directory/**/vendor/**/*.js"]-Dir["./search_directory/**/foundation/**/*.js"]).each do |file|
+# frozen_string_literal: true
+
+(Dir['./search_directory/**/*.vm'] +
+Dir['./search_directory/**/*.js'] -
+Dir['./search_directory/**/vendor/**/*.js'] -
+Dir['./search_directory/**/foundation/**/*.js']).each do |file|
   puts file
 end
