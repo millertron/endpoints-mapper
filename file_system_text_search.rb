88d68d6163dcd9cb58a8c2f6b8fc048fbf2f8afa
Millertron <senjinkoha@gmail.com>
2019-03-19 07:48:40 +0000

    Implement filesystem search using input json

--- file_system_text_search.rb
+++ file_system_text_search.rb
@@ -1,8 +1,37 @@
 # frozen_string_literal: true
+require 'json'
 
-(Dir['./search_directory/**/*.vm'] +
+file = File.read('input.json')
+endpoint_data = JSON.parse(file)
+
+view_files = (Dir['./search_directory/**/*.vm'] +
 Dir['./search_directory/**/*.js'] -
 Dir['./search_directory/**/vendor/**/*.js'] -
-Dir['./search_directory/**/foundation/**/*.js']).each do |file|
-  puts file
+Dir['./search_directory/**/foundation/**/*.js'])
+
+def get_controller_name str
+  str.split('.').last
 end
+
+endpoint_data.each do |endpoint|
+  key = endpoint.keys.first
+  controller = get_controller_name key
+  request_path = endpoint[key]['patternsCondition']['patterns'][0]
+  method = endpoint[key]['methodsCondition']['methods'][0] || 'GET'
+
+  search_text = request_path.gsub(/{[a-zA-Z]+}/, '*')[1..-1].gsub('/', '\/')
+
+  matched_files = []
+  view_files.each do |f|
+    File.open(f, 'r').each do |line|
+      if (line.match(/\b#{search_text}\b/i))
+        matched_files << f.gsub('./search_directory/', '')
+        break
+      end
+    end
+  end
+
+  puts "#{controller},#{request_path},#{method},#{matched_files.join(';')}"
+  
+end
+
